88b3c48588d0c4cb9a40d6c9e7e58d96b10f87fd
Stephane Bouget <stephane.bouget@orange.com>
2019-07-16 17:20:54 +0200

    #Collapse ok with bad perfs.

--- src/treeview.js
+++ src/treeview.js
@@ -107,7 +107,7 @@
 							icon.textContent = item.expanded ? 'folder' : 'folder_open';
 						}
 
-						text.textContent = item.name;
+						text.textContent = item.name + ' - ' + JSON.stringify(item.matrixIndex);
 						expando.setAttribute('class', 'tree-expando ' + (item.expanded ? 'expanded' : ''));
 						expando.textContent = item.expanded ? '-' : '+';
 						content.appendChild(expando);
